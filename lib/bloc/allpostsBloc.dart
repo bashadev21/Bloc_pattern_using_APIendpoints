3fa64ea08515aa6006806a2bb5e8cb794db95e3a
bashadev21 <basha28062@gmail.com>
2020-11-23 14:11:53 +0530

    ...

---
+++ lib/bloc/allpostsBloc.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+
+import 'package:blocApi/modals/allpostsmodal.dart';
+import 'package:blocApi/services/getAllPosts.dart';
+
+class AllPostsBloc {
+  final _allPostsController = StreamController<List<AllPosts>>();
+
+  Stream<List<AllPosts>> get allPostStream => _allPostsController.stream;
+  // Sink<List<AllPosts>> get allPostSink => _allPostsController.sink;
+  var _posts = <AllPosts>[];
+  List<AllPosts> data;
+
+  AllPostsBloc() {
+    print("hello");
+    posts();
+  }
+
+  posts() async {
+    data = await getAllPosts();
+    print(data);
+    _allPostsController.sink.add(data);
+  }
+
+  void dispose() {
+    _allPostsController.close();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:blocApi/screens/PostsScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // routes: {
+      //   '/postDetail': (context) => PostDetailScreen(),
+      // },
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PostsScreen();
+  }
+}
